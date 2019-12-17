0a250d87475dcc2c2ad8750a0234c4ca645cba0b
virb30 <virb30@gmail.com>
2019-12-17 09:46:26 -0300

    Code Refactor - Added Cache and Validators services

---
+++ src/app/services/CreateAppointmentService.js
@@ -0,0 +1,88 @@
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+import Notification from '../schemas/Notification';
+
+import Cache from '../../lib/Cache';
+
+class CreateAppointmentService {
+  async run({ provider_id, user_id, date }) {
+    /**
+     * Check if provider_id is a provider
+     */
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      throw new Error('You can only create appointments with providers');
+    }
+
+    /**
+     * Check if provider_id equals userId
+     */
+    if (checkIsProvider.id === user_id) {
+      throw new Error('You cannot create appointment to yourself');
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    /**
+     * Check if appointment date is in the past
+     */
+
+    if (isBefore(hourStart, new Date())) {
+      throw new Error('Past date are not permitted');
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      throw new Error('Appointment date is not available');
+    }
+
+    const appointment = await Appointment.create({
+      user_id,
+      provider_id,
+      date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(user_id);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
+    });
+
+    /**
+     * Invalidate Cache
+     */
+    await Cache.invalidatePrefix(`user:${user_id}:appointments`);
+
+    return appointment;
+  }
+}
+
+export default new CreateAppointmentService();
